558f2987bb24f810f3bf79024a566c47cbca412d
faisalt14 <faisalt14@gmail.com>
2024-10-12 16:30:06 -0400

    Initalized server, configured routes, and setup connection to redis.

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"Youtube-Learning-Mode-Quiz-Service/pkg/router"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	r := router.SetupRouter() // Set up the router
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8083" // Default port
+	}
+
+	log.Printf("Quiz service running on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r)) // Start server
+}
